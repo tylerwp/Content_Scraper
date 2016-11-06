f720c82f96ce8c830512cb140f15d57bbcd6a106
tylerwp <tylerwp@gmail.com>
2016-11-06 10:58:34 -0500

    created contentScraper & createCSV

--- scraper.js
+++ scraper.js
@@ -1,46 +1,101 @@
 var fs = require('fs');
+var json2csv = require('json2csv');
 
 //Check for data folder, create folder if it doesn't already exist.
-function checkDirectory(directory, callback) {  
-  fs.stat(directory, function(err, stats) {
-    //Check if error defined and the error code is "not exists"
-    if (err && err.errno === -4058) {
-      //Create the directory, call the callback.
-      fs.mkdir(directory, callback);
-    } else {
-      //just in case there was a different error:
-      callback(err)
-    }
-  });
+function checkDirectory(directory, callback) {
+    fs.stat(directory, function (err, stats) {
+        //Check if error defined and the error code is "not exists"
+        if (err && err.errno === -4058) {
+            //Create the directory, call the callback.
+            fs.mkdir(directory, callback);
+        } else {
+            //just in case there was a different error:
+            callback(err)
+        }
+    });
 }
 
-checkDirectory("./data/", function(error) {  
-  if(error) {
-    console.log("Error:", error);
-    //log error to csv
-  } else {
-    //Carry on, all good, directory exists / created.
-  }
+checkDirectory("./data/", function (error) {
+    if (error) {
+        console.log("Error:", error);
+        //log error
+    } else {
+        // run x-ray content scraper
+        contentScraper();
+
+    }
 });
 
+function contentScraper() {
+
+    //https://runkit.com/npm/x-ray
+
+    var Xray = require('x-ray');
+    var x = Xray();
+
+    x('http://shirts4mike.com', 'ul.products li', [{
+        url: 'a@href',
+        image: 'img@src',
+        price: x('a@href', '.price'),
+        title: x('a@href', '.shirt-details h1')
+    }])(function (err, result) {
+        //Create CSV 
+        if (err) {
+            // display message, write to log file
+            var error = err.code;
+        } else {
+            createCSV(result);
+        }
+    });
+
+}
 
 
-//https://runkit.com/npm/x-ray
-/*
-var Xray = require('x-ray');
-var x = Xray();
 
-x('http://shirts4mike.com', 'ul.products li', [{  
-  url: 'a@href',
-  image: 'img@src',
-  price:  x('a@href', '.price'),
-  title:  x('a@href', '.shirt-details h1')
-}])(function(err, result){
-    console.log(result)
-});
-https://runkit.com/npm/osmosis
-http://blog.webkid.io/nodejs-scraping-libraries/
-*/
+
+function createCSV(result) {
+
+    // create new CSV with date name. eg. 2016-01-29.csv
+    // https://www.npmjs.com/package/json2csv - https://github.com/zemirco/json2csv
+    var today = new Date();
+
+    var filename = today.getFullYear().toString() + '-' + today.getMonth().toString() + '-' + today.getDate().toString() + '.csv';
+
+
+    // reorder results and convert to JSON object
+    var JSONresults = resultsJSONFormatter(result);
+    // write to CSV file
+    var fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+    var csv = json2csv({ data: JSONresults, fields: fields });
+
+    fs.writeFile('data/' + filename, csv, 'utf8', function (err) {
+        if (err) throw err;
+        console.log('file saved');
+    });
+
+    // save/overwite csv file.
+
+}
+
+
+
+
+function resultsJSONFormatter(result) {
+
+    var json = [];
+
+    // Title, Price, ImageURL, URL and Time
+    result.forEach(function (entry) {
+
+        entry.title = entry.title.replace(entry.price, '').trim(); // remove price from title
+        var now = new Date();
+        entry.date = now.toString(); // add date to object
+        //{name: name, goals: goals[name]}
+        json.push({ 'Title': entry.title, 'Price': entry.price, 'ImageURL': entry.image, 'URL': entry.url, 'Time': entry.date });
+    });
+    return json;
+}
+
 
 
 /*
@@ -48,21 +103,21 @@ http://blog.webkid.io/nodejs-scraping-libraries/
 
 -- The scraper should create a folder called data, if a folder called data doesn't already exist (it should check for the folder).
 
-The information from the site you scrape should be stored in a CSV file named after today's date: 2016-01-29.csv.
+-- The information from the site you scrape should be stored in a CSV file named after today's date: 2016-01-29.csv.
 
 Use a third party npm package to scrape content from the site. As part of this assignment, you'll need to explain why you chose this package.
 
-The scraper should be able to visit the website http://shirts4mike.com and follow links to all t-shirts.
+-- The scraper should be able to visit the website http://shirts4mike.com and follow links to all t-shirts.
 
-The scraper should get the price, title, url and image url from the product page and save it in the CSV.
+-- The scraper should get the price, title, url and image url from the product page and save it in the CSV.
 
 Use a third party npm package to create an CSV file. As part of this assignment, you’ll need to explain why you chose this package.
 
-The column headers should be in this order: Title, Price, ImageURL, URL and Time. ‘Time’ should be the time the scrape happened. The columns must be in order (if we were really populating a database, the columns would need to be in order correctly populate the database).
+-- The column headers should be in this order: Title, Price, ImageURL, URL and Time. ‘Time’ should be the time the scrape happened. The columns must be in order (if we were really populating a database, the columns would need to be in order correctly populate the database).
 
 If the site is down, an error message describing the issue should appear in the console. You can test your error by disabling the wifi on your computer.
 
-If the data file for today’s date already exists, your program should overwrite the file.
+-- If the data file for today’s date already exists, your program should overwrite the file.
 
 Don't forget to document your code!
 
